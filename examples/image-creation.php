d514022d09dc627f8439e3104869e73db2de7089
Thomas Joußen <tjoussen91@gmail.com>
2023-08-24 21:52:45 +0200

    Cleanup example

--- examples/image-creation.php
+++ examples/image-creation.php
@@ -6,12 +6,6 @@ $API_KEY = $argv[1];
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$inputs = [
-    'I went to the store.',
-    'I went to the park.',
-    'I love bananas.',
-];
-
 $openAi = new OpenAi($API_KEY);
 $result = $openAi->images()->generation([
     'prompt' => 'An image of a real forest in the morning',
